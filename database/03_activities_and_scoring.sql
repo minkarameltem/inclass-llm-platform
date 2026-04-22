ba0dc60bcbb0f6f42291cd68229e1b71d90a3ca9
beyzazerdali <zerdalib@mef.edu.tr>
2026-04-22 22:35:28 +0300

    Update 03_activities_and_scoring.sql

--- database/03_activities_and_scoring.sql
+++ database/03_activities_and_scoring.sql
@@ -14,7 +14,7 @@ CREATE TABLE IF NOT EXISTS scores (
     student_email TEXT REFERENCES students(student_email) ON DELETE CASCADE,
     course_id TEXT REFERENCES courses(id) ON DELETE CASCADE,
     activity_no INT NOT NULL,
-    score FLOAT8 CHECK (score >= 0 AND score <= 100),
+    score FLOAT8 CHECK (score >= 0),
     meta JSONB,     is_achieved BOOLEAN DEFAULT FALSE, 
     graded_at TIMESTAMP WITH TIME ZONE DEFAULT NOW()
 );
